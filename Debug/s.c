1e519e7525d66d21af0cd338e23a31ab24e46032
Steve Estrada <stevee1@uci.edu>
2016-04-27 21:03:24 -0700

    Cleaned up and finished master shell

--- Debug/s.c
+++ Debug/s.c
@@ -1,24 +1,115 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-int main()
+#define LENGTH 80
+#define LINES 25
+#define MAX_ARGS 500
+#define SIGINT 2
+#define SIGCHLD 18
+#define SIGKILL 9
+
+int parse_line(char line[LENGTH], char cache[][LINES], char* args[]);
+
+void bg_handler(int sig) {
+	printf("Process %d received signal %d\n", getpid(), sig);
+	int stat, pid;
+	waitpid(-1, stat, WNOHANG);
+
+	printf("CHILD: %s : REAPED\n", pid);
+}
+
+int main(int argc, char * argv[])
 {
-	int pid = 0;
-	int child_status;
-	if ((pid = fork()) == 0)
+	signal(SIGCHLD, bg_handler);
+	//signal(SIGKILL, bg_handler);
+	while (1)
 	{
-		printf("FINISHED FORKING...\n");
-		printf("NOW EXITING...\n");
-		exit(0);
+		printf("prompt> ");
+		char line[LENGTH];
+		char cache[LENGTH][LINES];
+		char* args[MAX_ARGS];
+		int pid = 0;
+		int child_status;
+		int bg;
+		int bg_pid = 0;
+
+		fgets(line, LENGTH, stdin);
+		if ( (bg = parse_line(line, cache, args)) == -1)
+		{
+			continue;
+		}
+		if (strncmp(args[0], "quit", 4) == 0)
+		{
+			return 0;
+		}
+		else if (bg == 0)
+		{
+			//setpgid(0,0);
+			if ((bg_pid = fork()) == 0)
+			{
+				if (execvp(args[0], args) < 0 && execv(args[0], args) < 0)
+				{
+					printf("\nINVALID COMMAND: %s", args[0]);
+				}
+				printf("\nEXITING CHILD: %d", getpid());
+				printf("\nPID: %d\n", bg_pid);
+				exit(0);
+			}
+			else
+			{
+				printf("\nNO REAP YET\n");
+			}
+		}
+		else//forking
+		{
+			if ((pid = fork()) == 0)
+			{
+				if (execvp(args[0], args) < 0 && execv(args[0], args) < 0)
+				{
+					printf("\nINVALID COMMAND: %s", args[0]);
+				}
+				printf("\nEXITING CHILD: %d", getpid());
+				printf("\nPID: %d\n", pid);
+				exit(0);
+			}
+			else
+			{
+				int childid = wait(&child_status);
+				printf("\nREAPED CHILD: %d\n",childid);
+			}
+		}
+
 	}
-	else
-	{
-		printf("WILL REAP AFTER INPUT: \n");
-		char c = getchar();
-		pid_t wpid = wait(&child_status);
-		printf("REAPED THE CHILD...\n");
-	}
+
 	return 0;
 }
+int  parse_line(char line[LENGTH], char cache[][LINES], char* args[]) {
+
+	char * input = strtok(line, " \n\t");
+	if (input == NULL)
+		//null
+		return -1;
+	int i = 0;
+	while (input != NULL) {
+		strcpy(cache[i], input);
+		args[i] = cache[i];
+		++i;
+		input = strtok(NULL, " \n\t");
+	}
+	//background
+	if (strcmp(args[i - 1], "&") == 0) {
+		args[i - 1] = NULL;
+		return 0;
+	}
+	args[i] = NULL;
+	//foreground
+	return 1;
+}
+
+void cleanup()
+{
+	printf("Exiting Child");
+}
+
--- Master/Shell.c
+++ Master/Shell.c
@@ -1,10 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main()
+#define LENGTH 80
+#define LINES 25
+#define MAX_ARGS 500
+// FLAGS: 
+#define BG 0
+#define FG 1
+#define INVALID -1
+
+int parse_line(char line[LENGTH], char cache[][LINES], char* args[]);
+
+int main(int argc, char * argv[])
 {
+	int pid = 0;
+	int child_status;
+	while (1)
+	{
+		waitpid(-1, &child_status, WNOHANG);
+		printf("Dream> ");
+		char line[LENGTH];
+		char cache[LENGTH][LINES];
+		char* args[MAX_ARGS];
 
+		fgets(line, LENGTH, stdin);
+		int value = parse_line(line, cache, args);
 
-	getchar();
+		if (value == INVALID)
+			continue;
+
+		if (strcmp(args[0], "quit") == 0)
+		{
+			return 0;
+		}
+		else//forking
+		{
+			if ((pid = fork()) == 0)
+			{
+				if (execvp(args[0], args) < 0 && execv(args[0], args) < 0)
+				{
+					printf("%s command not found", args[0]);
+					exit(0);
+				}
+			}
+			else
+			{
+				printf("hello from the parent\n");
+				if (value == FG)
+					wait(&child_status);
+				else if (value == BG) {
+					printf("Running in background ");
+					int return_value = waitpid(pid, &child_status, WNOHANG);
+					if (return_value < 0)
+						printf("Error: %s", strerror(return_value));
+				}
+				printf("CT: child has terminated\n");
+			}
+		}
+
+	}
 	return 0;
 }
+
+int  parse_line(char line[LENGTH], char cache[][LINES], char* args[])
+{
+	char * input = strtok(line, " \n\t");
+	if (input == NULL)
+		return -1;
+	int i = 0;
+	while (input != NULL)
+	{
+		strcpy(cache[i], input);
+		args[i] = cache[i];
+		++i;
+		input = strtok(NULL, " \n\t");
+	}
+
+	int p_type;
+
+	if (strcmp(args[i - 1], "&") == 0)
+	{//background process
+		args[i - 1] = NULL;
+		p_type = BG;
+	}
+	else   //foreground process
+	{
+		args[i] = NULL;
+		p_type = FG;
+	}
+
+	return p_type;
+}
+
